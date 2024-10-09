13f49e414efe2a6faef138217bb1d8feae8b0532
juanjolr7 <juanjolr6@gmail.com>
2024-10-09 14:24:54 -0600

    creacion del proyecto

---
+++ Compilador/src/main/java/com/mycompany/compilador/Compilador.java
@@ -0,0 +1,12 @@
+package com.mycompany.compilador;
+
+/**
+ *
+ * @author Juan José López Rosa
+ */
+public class Compilador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
