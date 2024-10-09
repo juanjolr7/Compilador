8f0ff1bf00f3bac9d36ccc83c461646848d95c07
juanjolr7 <juanjolr6@gmail.com>
2024-10-09 14:33:49 -0600

    Hola mundo

--- Compilador/src/main/java/com/mycompany/compilador/Compilador.java
+++ Compilador/src/main/java/com/mycompany/compilador/Compilador.java
@@ -7,6 +7,6 @@ package com.mycompany.compilador;
 public class Compilador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
